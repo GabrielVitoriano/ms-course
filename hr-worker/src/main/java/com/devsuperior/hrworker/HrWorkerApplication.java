668a6686d93c0be95d7aa887fa6bf6a61b758158
GabrielVitoriano <138249302+GabrielVitoriano@users.noreply.github.com>
2023-08-04 17:29:47 -0300

    Progresso no projeto de microsserviço

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -1,13 +1,26 @@
 package com.devsuperior.hrworker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class HrWorkerApplication {
+public class HrWorkerApplication  {
 
+	/*@Value("spring.cloud.config.server.git.username")
+	private String username;*/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
 	}
+
+	/*@Override
+	public void run(String... args) throws Exception {
+		System.out.println("USERNAME = " + username);
+		
+	}*/
 
 }
