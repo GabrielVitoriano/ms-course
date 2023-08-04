c8bf4552ee1e954e27bb507dd7b97f3775851196
GabrielVitoriano <138249302+GabrielVitoriano@users.noreply.github.com>
2023-08-04 17:31:53 -0300

    Sistema de pagamentos

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,17 @@
+package com.devsuperior.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableCircuitBreaker
+@EnableFeignClients
+@SpringBootApplication
+public class HrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollApplication.class, args);
+	}
+
+}
